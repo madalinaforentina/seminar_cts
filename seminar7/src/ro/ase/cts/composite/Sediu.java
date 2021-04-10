31f73518914bc03212030714d9f0762017142caa
madalinaforentina <carutasumadalina18@stud.ase.ro>
2021-04-10 15:13:40 +0300

    Composit

---
+++ seminar7/src/ro/ase/cts/composite/Sediu.java
@@ -0,0 +1,10 @@
+package ro.ase.cts.composite;
+
+public interface Sediu {
+	void descriere();
+	void adaugaSediu(Sediu sediu) throws Exception;
+	void stergeSediu(Sediu sediu) throws Exception;
+	Sediu getSediu(int pozitie) throws Exception;
+	
+
+}
--- seminar7/src/ro/ase/cts/facade/main/Main.java
+++ seminar7/src/ro/ase/cts/facade/main/Main.java
@@ -29,5 +29,4 @@ public class Main {
 			System.out.println("cont necreat");
 		}
 	}
-
 }
